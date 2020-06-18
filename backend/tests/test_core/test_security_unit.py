6dfcf9e02c18b58bd1f9a0d39f96c59dcb335c51
Ryan Bradshaw <ryan@rbradshaw.dev>
2020-06-18 22:52:20 +0100

    added security test

---
+++ backend/tests/test_core/test_security_unit.py
@@ -0,0 +1,12 @@
+from app.core.security import create_access_token
+
+
+def test_create_access_token():
+    token = create_access_token(
+        data={"sub": "foooooo"},
+        expiry=10,
+        key="its_shhhhuuuper_secret",
+        algorithm="HS256"
+    )
+
+    assert isinstance(token, bytes)
